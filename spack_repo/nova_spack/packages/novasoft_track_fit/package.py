b587e3e28f04b04a864de9d0c2841f33945aa0ac
Robin James <rnjames@wm.edu>
2026-08-23 18:58:28 -0400

    Add MCCheater dependency to TrackFit

--- spack_repo/nova_spack/packages/novasoft_track_fit/package.py
+++ spack_repo/nova_spack/packages/novasoft_track_fit/package.py
@@ -7,6 +7,7 @@ class NovasoftTrackFit(NovasoftPackage):
     for dep in ("art", "art-root-io", "canvas", "cetlib", "cetlib-except", "fhicl-cpp",
                 "messagefacility", "nova-daq", "novasoft-calibrator", "novasoft-channel-info",
                 "novasoft-cmap", "novasoft-geometry", "novasoft-geometry-objects",
-                "novasoft-live-geometry", "novasoft-raw-data", "novasoft-reco-base", "novasoft-simulation",
+                "novasoft-live-geometry", "novasoft-mccheater", "novasoft-raw-data",
+                "novasoft-reco-base", "novasoft-simulation",
                 "novasoft-utilities", "nusimdata", "root"):
         depends_on(dep)
