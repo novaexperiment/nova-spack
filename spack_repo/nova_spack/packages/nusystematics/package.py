131d113defd08faa0b8d3b7adeb7fb0895a56817
Robin James <rnjames@wm.edu>
2026-08-25 11:00:55 -0400

    Add Eigen dependency to NuSystematics

---
+++ spack_repo/nova_spack/packages/nusystematics/package.py
@@ -0,0 +1,10 @@
+from spack_repo.fnal_art.packages.nusystematics.package import (
+    Nusystematics as FnalNusystematics,
+)
+from spack.package import depends_on
+
+
+class Nusystematics(FnalNusystematics):
+    """NuSystematics with dependencies required by current releases."""
+
+    depends_on("eigen@3.4:")
